25e89eab0fb26bcca694ebd5fa2f1bff008e85d1
Ricardo <>
2020-12-18 20:29:04 +0700

    added middleware

--- routes/web.php
+++ routes/web.php
@@ -16,17 +16,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::middleware(['role:member'])->group( function(){
-    Route::get('/cart', function() {
-        return view('cart');
+// harus login
+Route::middleware(['auth'])->group( function () { 
+    // role admin/member
+    Route::middleware(['role:admin|member'])->group( function(){
+        
     });
-    Route::get('/history', function() {
-        return view('history');
+    // role member
+    Route::middleware(['role:member'])->group( function(){
+        Route::get('/cart', function() {
+            return view('cart');
+        });
+        Route::get('/history', function() {
+            return view('history');
+        });
     });
-});
-Route::middleware(['role:admin'])->group( function(){
-    
-});
+    //role admin
+    Route::middleware(['role:admin'])->group( function(){
+        
+    });
+} );
+
 Route::get('/gardener', "GardenersController@index");
 Route::get('/store', "PlantsController@index");
 Route::get('/store/{plant}', "PlantsController@show");
