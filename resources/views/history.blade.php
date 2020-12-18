d827a4c6b40c15f13df0198b6230fbc001093850
Ricardo <>
2020-12-18 19:02:13 +0700

    Added title on cart

---
+++ resources/views/history.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('styles')
+    <link rel="stylesheet" href="{{ asset('css/history.css') }}">
+@endsection
+
+@section('content')
+
+<div class="custom-container">
+    
+
+</div>
+
+@endsection
