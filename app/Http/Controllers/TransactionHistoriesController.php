643076e89c593556b8334ac5f14d70fa08e8a926
cafaus05 <cafaus.05@gmail.com>
2020-12-20 16:06:38 +0700

    fix history view

--- app/Http/Controllers/TransactionHistoriesController.php
+++ app/Http/Controllers/TransactionHistoriesController.php
@@ -3,23 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Auth;
 class TransactionHistoriesController extends Controller
 {
     public function index(){
-        $transactionHistories = \App\TransactionHistory::all();
+        $user = Auth::user();
+        $transactionHistories = \App\TransactionHistory::where('user_id',$user->id)->get();
         $subTotal = [];
         
-        foreach ($transactionHistories as $transactionHistory) {
-            $total = 0;
-            foreach ($transactionHistory->plantTransactionHistories as $plantTransactionHistory){
-                $total += $plantTransactionHistory->totalPrice;
+        if($transactionHistories){
+            foreach ($transactionHistories as $transactionHistory) {
+                $total = 0;
+                foreach ($transactionHistory->plantTransactionHistories as $plantTransactionHistory){
+                    $total += $plantTransactionHistory->totalPrice;
+                }
+                
+                foreach ($transactionHistory->gardenerTransactionHistories as $gardenerTransactionHistory){
+                    $total += $gardenerTransactionHistory->totalPrice;
+                }
+                array_push($subTotal, $total);
             }
-            
-            foreach ($transactionHistory->gardenerTransactionHistories as $gardenerTransactionHistory){
-                $total += $gardenerTransactionHistory->totalPrice;
-            }
-            array_push($subTotal, $total);
         }
         
         return view('history', compact('transactionHistories','subTotal'));
